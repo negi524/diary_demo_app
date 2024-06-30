a4a99f46165b9297332d6a7ae6539d90fa5d4877
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-06-30 15:03:12 +0900

    feat: add global navigation (#2)

---
+++ lib/home_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+/// ホームページ
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Home Page');
+  }
+}
