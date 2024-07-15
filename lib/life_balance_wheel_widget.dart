108d88e97983cb03d1217a56bfa5afc4f0ddd103
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-07-16 08:46:46 +0900

    feat: ライフバランスホイールを表形式で表示

--- lib/life_balance_wheel_widget.dart
+++ lib/life_balance_wheel_widget.dart
@@ -1,3 +1,4 @@
+import 'package:diary_demo_app/domain/life_balance_wheel.dart';
 import 'package:diary_demo_app/main.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -19,7 +20,51 @@ class _LifeBalanceWheelWidgetState extends State<LifeBalanceWheelWidget> {
         padding: const EdgeInsets.all(20),
         child: Text('ライフバランスホイール'),
       ),
-      Text('${lifeBalanceWheel.family.getName()}='),
+      buildTable(lifeBalanceWheel),
     ]);
   }
+}
+
+Widget buildTable(LifeBalanceWheel lifeBalanceWheel) {
+  return Table(
+    border: TableBorder.all(),
+    columnWidths: {
+      0: FixedColumnWidth(3),
+      1: FixedColumnWidth(1),
+    },
+    children: [
+      buildTableRow(lifeBalanceWheel.workSatisfaction),
+      buildTableRow(lifeBalanceWheel.relationships),
+      buildTableRow(lifeBalanceWheel.family),
+      buildTableRow(lifeBalanceWheel.lovePartnership),
+      buildTableRow(lifeBalanceWheel.fitnessHealth),
+      buildTableRow(lifeBalanceWheel.hobbiesEntertainment),
+      buildTableRow(lifeBalanceWheel.money),
+      buildTableRow(lifeBalanceWheel.livingEnvironment),
+    ],
+  );
+}
+
+TableRow buildTableRow(LifeBalanceAsset asset) {
+  return TableRow(
+    children: [
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(asset.getName()),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextFormField(
+          initialValue: '0',
+          keyboardType: TextInputType.number,
+          onChanged: (value) {
+            // TODO: 保存処理実装
+          },
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+          ),
+        ),
+      )
+    ],
+  );
 }
