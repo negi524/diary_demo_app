6acdd2db6ecf2bf611b623578628b7b6ed7c7a2d
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-07-15 12:28:13 +0900

    feat: 備考フィールド追加

--- lib/domain/week_goal.dart
+++ lib/domain/week_goal.dart
@@ -1,7 +1,13 @@
 /// 週の目標
 class WeekGoal {
+  /// 理想の状態
   final String idealState;
+
+  /// 理想の状態になるためにやるべきこと
   final List<String> todoList;
 
-  WeekGoal({required this.idealState, required this.todoList});
+  /// 備考
+  final String? remarks;
+
+  WeekGoal({required this.idealState, required this.todoList, this.remarks});
 }
--- lib/domain/week_review.dart
+++ lib/domain/week_review.dart
@@ -1,5 +1,6 @@
 /// 週の振り返り
 class WeekReview {
+  /// 振り返りのテキスト
   final String reviewText;
 
   WeekReview({required this.reviewText});
