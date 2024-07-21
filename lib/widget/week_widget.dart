a667ea876f345ee00435749389fcc05eae3dcb5b
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-07-21 14:06:57 +0900

    refactor: 週の振り返りウィジェットを切り出し

--- lib/widget/week_widget.dart
+++ lib/widget/week_widget.dart
@@ -1,6 +1,6 @@
-import 'package:diary_demo_app/page/review_edit_page.dart';
 import 'package:diary_demo_app/state/week_goal_review_state.dart';
 import 'package:diary_demo_app/widget/week_goal_widget.dart';
+import 'package:diary_demo_app/widget/week_review_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -22,21 +22,7 @@ class WeekWidget extends StatelessWidget {
           SizedBox(height: 30),
           WeekGoalWidget(weekGoal: weekState.getWeekGoal()),
           SizedBox(height: 60),
-          Row(children: [
-            Text('振り返り', style: Theme.of(context).textTheme.headlineLarge),
-            SizedBox(width: 30),
-            ElevatedButton(
-                onPressed: () {
-                  // ボタンが押されたときの処理
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => ReviewEditPage()));
-                },
-                child: Text('振り返り編集')),
-          ]),
-          SizedBox(height: 30),
-          Text(weekState.getWeekReviewText()),
+          WeekReviewWidget(weekReview: weekState.getWeekReview()),
         ],
       ),
     );
