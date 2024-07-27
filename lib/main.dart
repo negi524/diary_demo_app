c00da06ac7863c4ffe56b68ad4eb392c8318f0b1
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-07-27 09:09:06 +0900

    feat: flutter pub add firebase_auth

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   Firebase.initializeApp(
