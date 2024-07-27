a254d32d2133cb907207ba1bb99abd3f6f6e8255
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-07-27 09:19:58 +0900

    feat: FirebaseAuthのメソッド利用

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
-  Firebase.initializeApp(
+void main() async {
+  await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   setUpLocator();
--- lib/widget/profile_widget.dart
+++ lib/widget/profile_widget.dart
@@ -1,9 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 /// 設定ページ
 class ProfileWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        print('hoge');
+      } else {
+        print('fuga');
+      }
+    });
     return Text('Profile widget',
         style: Theme.of(context).textTheme.headlineLarge);
     // throw UnimplementedError();
