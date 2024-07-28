48aef0764cf196a043ec59a8d0069419b2532028
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-07-28 14:43:38 +0900

    feat: ログインフォームのハリボテ実装

--- lib/widget/profile_widget.dart
+++ lib/widget/profile_widget.dart
@@ -15,35 +15,73 @@ class ProfileWidget extends StatelessWidget {
         print('fuga');
       }
     });
-    return Column(
-      children: [
-        Row(
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  userState.signIn();
-                },
-                child: Text('sign in')),
-            ElevatedButton(
-                onPressed: () {
-                  userState.signOut();
-                },
-                child: Text('sign out')),
-          ],
-        ),
-        Row(
-          children: [
-            Text('ユーザーID: '),
-            Text(userState.getUserId()),
-          ],
-        ),
-        Row(
-          children: [
-            Text('ユーザー名: '),
-            Text(userState.getUserName()),
-          ],
-        ),
-      ],
-    );
+    if (userState.isLogin()) {
+      return buildSignOutForm(userState);
+    } else {
+      return buildSignInForm(userState);
+    }
   }
+}
+
+/// サインアウト用のボタンを表示する
+Widget buildSignOutForm(UserState userState) {
+  return Column(
+    children: [
+      Row(
+        children: [
+          ElevatedButton(
+              onPressed: () {
+                userState.signOut();
+              },
+              child: Text('sign out'))
+        ],
+      ),
+      Row(
+        children: [
+          Text('ユーザーID: '),
+          Text(userState.getUserId()),
+        ],
+      ),
+      Row(
+        children: [
+          Text('ユーザー名: '),
+          Text(userState.getUserName()),
+        ],
+      ),
+    ],
+  );
+}
+
+/// サインイン用のフォームを表示する
+Widget buildSignInForm(UserState userState) {
+  // TODO: 変数で持つ
+  return Column(
+    children: [
+      TextField(
+        decoration: InputDecoration(
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(4.0))),
+            hintText: 'メールアドレス'),
+        keyboardType: TextInputType.emailAddress,
+        onChanged: (mail) {
+          // TODO: メールアドレス同期
+        },
+      ),
+      TextField(
+        decoration: InputDecoration(
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(4.0))),
+            hintText: 'パスワード'),
+        keyboardType: TextInputType.visiblePassword,
+        onChanged: (mail) {
+          // TODO: パスワード同期
+        },
+      ),
+      ElevatedButton(
+          onPressed: () {
+            userState.signIn();
+          },
+          child: Text('sign in'))
+    ],
+  );
 }
